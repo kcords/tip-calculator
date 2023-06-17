1dff3b0b0c52617fa8a6cd125e1e399b7eeb6e05
Kyle Cordell <kcordell-git@outlook.com>
2023-06-17 14:39:16 -0700

    Adds primary logic and formatting

--- js/index.js
+++ js/index.js
@@ -1,48 +1,116 @@
-// Shows/hides quality of service / percentage groups depending on selection
-const calculateBySelect = document.getElementById("calculateBy");
+let tipMultiplier = 0;
+let subtotal = 0;
+let tipTotal = 0;
 
-calculateBySelect.addEventListener("input", ({ target: { value } }) => {
-  const qualityOfServiceElements =
-    document.getElementsByClassName("quality-of-service");
+const subtotalSelect = document.querySelector("#subtotal");
+const calculateBySelect = document.querySelector("#calculateBy");
+const percentageSelect = document.querySelector("#percentage");
+const percentageDisplayedInput = document.querySelector(
+  "#percentage-displayed"
+);
+const qualityOfServiceSelect = document.querySelector("#qualityOfService");
+const qualityOfServiceElements = document.querySelectorAll(
+  ".quality-of-service"
+);
+const percentageGroupElements = document.querySelectorAll(".percentage-group");
+const roundToInput = document.querySelector("#roundTo");
+const tipInput = document.querySelector("#tip");
+const totalInput = document.querySelector("#total");
+
+const qualityOfServiceValues = {
+  incredible: { value: 25, emoji: "&#x1F929" },
+  good: { value: 20, emoji: "&#x1F603" },
+  okay: { value: 18, emoji: "&#x1F610" },
+  poor: { value: 12, emoji: "&#x1F61E" },
+};
+
+percentageDisplayedInput.value = percentageSelect.value;
+
+for (const qualityOfServiceOption in qualityOfServiceValues) {
+  const option = document.createElement("option");
+  option.value = qualityOfServiceOption;
+  option.innerHTML = `${qualityOfServiceValues[qualityOfServiceOption].emoji} ${qualityOfServiceOption}`;
+  if (qualityOfServiceOption === "good") option.setAttribute("selected", "");
+  option.classList.add("capitalized");
+  qualityOfServiceSelect.appendChild(option);
+}
+
+subtotalSelect.addEventListener("input", () => {
+  updateSubtotal();
+  updateTipMultiplier();
+});
+
+calculateBySelect.addEventListener("change", ({ target: { value } }) => {
+  toggleCalculateByElementsVisibility(value);
+  updateTipMultiplier();
+});
+
+qualityOfServiceSelect.addEventListener("change", () => {
+  updateTipMultiplier();
+});
+
+percentageSelect.addEventListener("input", ({ target: { value } }) => {
+  percentageDisplayedInput.value = value;
+  updateTipMultiplier();
+});
+
+roundToInput.addEventListener("change", () => {
+  //TODO
+});
+
+function toggleCalculateByElementsVisibility(value) {
   for (const element of qualityOfServiceElements) {
     value === "service-quality"
       ? element.removeAttribute("hidden")
       : element.setAttribute("hidden", "");
   }
 
-  const percentageGroupElements =
-    document.getElementsByClassName("percentage-group");
   for (const element of percentageGroupElements) {
     value === "percentage"
       ? element.removeAttribute("hidden")
       : element.setAttribute("hidden", "");
   }
-});
-
-// Creates a list of values for qualityOfService that correspond to predefined tip percentage recommendations
-const qualityOfServiceSelect = document.getElementById("qualityOfService");
-
-const qualityOfServiceValues = {
-  incredible: 25,
-  good: 20,
-  decent: 18,
-  poor: 12,
-};
-
-for (const qualityOfServiceOption in qualityOfServiceValues) {
-  const option = document.createElement("option");
-  option.value = qualityOfServiceOption;
-  option.textContent = qualityOfServiceOption;
-  option.classList.add("capitalized");
-  qualityOfServiceSelect.appendChild(option);
 }
 
-// Sets percentageDisplayed to match percentageSelect value
-const percentageSelect = document.querySelector("#percentage");
-const percentageDisplayed = document.querySelector("#percentage-displayed");
+function calculatePercentageMultiplier(percentValue) {
+  return percentValue * 0.01;
+}
 
-percentageDisplayed.value = percentageSelect.value;
+function updateTipMultiplier() {
+  const newTipPercentage =
+    calculateBySelect.value === "percentage"
+      ? percentageSelect.value
+      : qualityOfServiceValues[qualityOfServiceSelect.value].value;
 
-percentageSelect.addEventListener("change", ({ target: { value } }) => {
-  percentageDisplayed.value = value;
-});
+  tipMultiplier = calculatePercentageMultiplier(newTipPercentage);
+  console.log({ tipMultiplier });
+  updateTipTotal();
+}
+
+function calculateTipTotal() {
+  return subtotal * tipMultiplier;
+}
+
+function updateTipTotal() {
+  tipTotal = calculateTipTotal();
+  console.log({ tipTotal }, typeof tipTotal);
+  updateTipInput();
+  updateTotalInput();
+}
+
+function updateSubtotal() {
+  subtotal = parseInt(subtotalSelect.value);
+  console.log({ subtotal }, typeof subtotal);
+  updateTipInput();
+  updateTotalInput();
+}
+
+function updateTipInput() {
+  tipInput.value = tipTotal.toFixed(2);
+}
+
+function updateTotalInput() {
+  console.log({ subtotal, tipTotal }, subtotal + tipTotal);
+  calculateRoundedOffset();
+  totalInput.value = Number(subtotal + tipTotal).toFixed(2);
+}
